7865707a2fdfb32e7fc14f9f2e34df19728672ff
hjj2017 <hjj2017@163.com>
2015-03-28 08:28:36 +0800

    更新消息接收过程；

--- game_server/src/com/game/gameServer/framework/MINA_IoHandler.java
+++ game_server/src/com/game/gameServer/framework/MINA_IoHandler.java
@@ -5,8 +5,8 @@ import org.apache.mina.core.session.IoSession;
 
 import com.game.gameServer.msg.AbstractCGMsgHandler;
 import com.game.gameServer.msg.AbstractCGMsgObj;
+import com.game.gameServer.msg.SpecialMsgSerialUId;
 import com.game.part.msg.MsgServ;
-import com.game.part.msg.type.AbstractMsgObj;
 import com.game.part.util.Assert;
 
 /**
@@ -21,6 +21,11 @@ class MINA_IoHandler extends IoHandlerAdapter {
 	public void sessionCreated(IoSession sessionObj) {
 		// 断言参数对象不为空
 		Assert.notNull(sessionObj);
+
+		// 获取消息对象
+		AbstractCGMsgObj<?> cgMsgObj = MsgServ.OBJ.newMsgObj(SpecialMsgSerialUId.SESSION_OPENED);
+		// 发送消息
+		this.postMsg(sessionObj, cgMsgObj);
 	}
 
 	@Override
@@ -30,24 +35,47 @@ class MINA_IoHandler extends IoHandlerAdapter {
 		Assert.notNull(obj);
 
 		if (obj instanceof AbstractCGMsgObj) {
-			// 转型为 CGMsg
-			AbstractCGMsgObj<?> cgMsgObj = (AbstractCGMsgObj<?>)obj;
-			// 设置消息处理器
-			AbstractCGMsgHandler<?> handler = cgMsgObj.getSelfHandler();
-
-			if (handler != null) {
-				// 设置会话 Id
-				handler._sessionId = sessionObj.getId();
-			}
+			// 发送消息
+			this.postMsg(sessionObj, (AbstractCGMsgObj<?>)obj);
 		}
-
-		// 分派消息对象
-		MsgServ.OBJ.post((AbstractMsgObj)obj);
 	}
 
 	@Override
 	public void sessionClosed(IoSession sessionObj) {
 		// 断言参数对象不为空
 		Assert.notNull(sessionObj);
+
+		// 获取消息对象
+		AbstractCGMsgObj<?> cgMsgObj = MsgServ.OBJ.newMsgObj(SpecialMsgSerialUId.SESSION_CLOSED);
+		// 发送消息
+		this.postMsg(sessionObj, cgMsgObj);
+	}
+
+	/**
+	 * 发送消息对象
+	 * 
+	 * @param fromSessionObj
+	 * @param cgMsgObj
+	 * 
+	 */
+	private void postMsg(
+		IoSession fromSessionObj, AbstractCGMsgObj<?> cgMsgObj) {
+		if (fromSessionObj == null || 
+			cgMsgObj == null) {
+			// 如果参数对象为空, 
+			// 则直接退出!
+			return;
+		}
+
+		// 设置消息处理器
+		AbstractCGMsgHandler<?> handler = cgMsgObj.getSelfHandler();
+
+		if (handler != null) {
+			// 设置会话 Id
+			handler._sessionId = fromSessionObj.getId();
+		}
+
+		// 分派消息对象
+		MsgServ.OBJ.post(cgMsgObj);
 	}
 }
--- game_server/src/com/game/gameServer/msg/SpecialMsgSerialUId.java
+++ game_server/src/com/game/gameServer/msg/SpecialMsgSerialUId.java
@@ -8,17 +8,21 @@ package com.game.gameServer.msg;
  * 
  */
 public final class SpecialMsgSerialUId {
-	/**
-	 * 类默认构造器
-	 * 
-	 */
-	private SpecialMsgSerialUId() {
-	}
-
 	/** 由 Flash Socket 发送的 Policy Request 请求协议 "&lt;policy" 中第 3, 4 两个字节 "ol" 的16进制表示 : 28524 */
 	public static final short CG_FLASH_POLICY = 0x6f6c;
 	/** 腾讯网关消息 */
 	public static final short CG_QQ_TGW = 0x775f;
 	/** 腾讯网关消息 */
 	public static final short GC_QQ_TGW = 51;
+	/** 会话开启 */
+	public static final short SESSION_OPENED = 1;
+	/** 会话关闭 */
+	public static final short SESSION_CLOSED = 44;
+
+	/**
+	 * 类默认构造器
+	 * 
+	 */
+	private SpecialMsgSerialUId() {
+	}
 }
