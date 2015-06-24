4d338ba54210bb6778f657ae722f384894606d8b
海江 <afritxia@gmail.com>
2015-06-24 19:53:46 +0800

    更新初始化 SQL;

--- passport_server/init.sql
+++ passport_server/init.sql
@@ -1,13 +1,93 @@
 set names 'utf8';
 
 -- 创建数据表
-CREATE TABLE `t_passport` (
-  `qid` bigint(20) NOT NULL,
+CREATE TABLE `t_passport_0` (
+  `platform_uuid` varchar(64) NOT NULL,
   `create_time` bigint(20) DEFAULT NULL,
   `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `last_login_time` bigint(20) DEFAULT NULL,
   `last_game_server_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`qid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-/*!50100 PARTITION BY HASH (qid)
-PARTITIONS 256 */
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_1` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_2` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_3` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+CREATE TABLE `t_passport_4` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_5` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_6` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_7` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_8` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_passport_9` (
+  `platform_uuid` varchar(64) NOT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `pf` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `last_login_time` bigint(20) DEFAULT NULL,
+  `last_game_server_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`platform_uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
