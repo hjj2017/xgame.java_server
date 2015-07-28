37631f24a87630f72fd9199d5caf1ac7bf605073
hjj2017 <hjj2017@163.com>
2015-07-29 07:56:00 +0800

    修改建表语句;

--- all_config/db/X_s00_game.sql
+++ all_config/db/X_s00_game.sql
@@ -1,9 +1,8 @@
 /*
 SQLyog Community v11.32 (64 bit)
-MySQL - 5.6.24 : Database - X_s00_game
+MySQL - 5.6.24 : Database - x_s00_game
 *********************************************************************
 */
-
 
 /*!40101 SET NAMES utf8 */;
 
@@ -13,9 +12,186 @@ MySQL - 5.6.24 : Database - X_s00_game
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`X_s00_game` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`x_s00_game` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `X_s00_game`;
+
+/*Table structure for table `t_building` */
+
+DROP TABLE IF EXISTS `t_building`;
+
+CREATE TABLE `t_building` (
+  `human_uid` bigint(20) NOT NULL,
+  `building_1_level` int(11) DEFAULT NULL,
+  `building_2_level` int(11) DEFAULT NULL,
+  `building_3_level` int(11) DEFAULT NULL,
+  `building_4_level` int(11) DEFAULT NULL,
+  `building_5_level` int(11) DEFAULT NULL,
+  `building_6_level` int(11) DEFAULT NULL,
+  `building_7_level` int(11) DEFAULT NULL,
+  `building_8_level` int(11) DEFAULT NULL,
+  PRIMARY KEY (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `t_cd_timer_0` */
+
+DROP TABLE IF EXISTS `t_cd_timer_0`;
+
+CREATE TABLE `t_cd_timer_0` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_1` */
+
+DROP TABLE IF EXISTS `t_cd_timer_1`;
+
+CREATE TABLE `t_cd_timer_1` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_2` */
+
+DROP TABLE IF EXISTS `t_cd_timer_2`;
+
+CREATE TABLE `t_cd_timer_2` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_3` */
+
+DROP TABLE IF EXISTS `t_cd_timer_3`;
+
+CREATE TABLE `t_cd_timer_3` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_4` */
+
+DROP TABLE IF EXISTS `t_cd_timer_4`;
+
+CREATE TABLE `t_cd_timer_4` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_5` */
+
+DROP TABLE IF EXISTS `t_cd_timer_5`;
+
+CREATE TABLE `t_cd_timer_5` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_6` */
+
+DROP TABLE IF EXISTS `t_cd_timer_6`;
+
+CREATE TABLE `t_cd_timer_6` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_7` */
+
+DROP TABLE IF EXISTS `t_cd_timer_7`;
+
+CREATE TABLE `t_cd_timer_7` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_8` */
+
+DROP TABLE IF EXISTS `t_cd_timer_8`;
+
+CREATE TABLE `t_cd_timer_8` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_cd_timer_9` */
+
+DROP TABLE IF EXISTS `t_cd_timer_9`;
+
+CREATE TABLE `t_cd_timer_9` (
+  `uid_str` varchar(64) NOT NULL,
+  `human_uid` bigint(20) DEFAULT NULL,
+  `cd_type_int` int(11) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `end_time` bigint(20) DEFAULT NULL,
+  `opened` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`uid_str`),
+  UNIQUE KEY `human_uuid_2` (`human_uid`,`cd_type_int`),
+  KEY `human_uuid` (`human_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*Table structure for table `t_human` */
 
@@ -23,16 +199,16 @@ DROP TABLE IF EXISTS `t_human`;
 
 CREATE TABLE `t_human` (
   `human_uid` bigint(20) NOT NULL DEFAULT '0',
-  `platform_uid_str` varchar(64) DEFAULT NULL,
-  `full_name` varchar(48) DEFAULT NULL,
   `human_name` varchar(32) DEFAULT NULL,
+  `platform_uid_str` varchar(64) DEFAULT NULL,
   `server_name` varchar(16) DEFAULT NULL,
   `human_level` int(11) DEFAULT NULL,
   `gold` int(11) DEFAULT NULL,
+  `full_name` varchar(48) DEFAULT NULL,
   PRIMARY KEY (`human_uid`),
-  KEY `platform_uid_str` (`platform_uid_str`),
   UNIQUE KEY `full_name` (`full_name`),
-  KEY `human_name` (`human_name`)
+  KEY `human_name` (`human_name`),
+  KEY `platform_uid_str` (`platform_uid_str`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*Table structure for table `t_player` */
