da87806ab58e75de5d74b15586151e6c46195ba7
hjj2017 <hjj2017@163.com>
2016-01-08 14:55:28 +0800

    更新异常代码；

--- game_part/src/main/java/com/game/part/BadArgError.java
+++ game_part/src/main/java/com/game/part/BadArgError.java
@@ -7,45 +7,45 @@ package com.game.part;
  *
  */
 public class BadArgError extends GameError {
-	/** serialVersionUID */
-	private static final long serialVersionUID = 7400420633310393852L;
+    /** serialVersionUID */
+    private static final long serialVersionUID = 7400420633310393852L;
 
-	/**
-	 * 类默认构造器
-	 * 
-	 */
-	public BadArgError() {
-		super();
-	}
+    /**
+     * 类默认构造器
+     *
+     */
+    public BadArgError() {
+        super();
+    }
 
-	/**
-	 * 类参数构造器
-	 * 
-	 * @param msg
-	 * 
-	 */
-	public BadArgError(String msg) {
-		super(msg);
-	}
+    /**
+     * 类参数构造器
+     *
+     * @param msg
+     *
+     */
+    public BadArgError(String msg) {
+        super(msg);
+    }
 
-	/**
-	 * 类参数构造器
-	 * 
-	 * @param err
-	 * 
-	 */
-	public BadArgError(Throwable err) {
-		super(err);
-	}
+    /**
+     * 类参数构造器
+     *
+     * @param err
+     *
+     */
+    public BadArgError(Throwable err) {
+        super(err);
+    }
 
-	/**
-	 * 类参数构造器
-	 * 
-	 * @param msg
-	 * @param err
-	 * 
-	 */
-	public BadArgError(String msg, Throwable err) {
-		super(msg, err);
-	}
+    /**
+     * 类参数构造器
+     *
+     * @param msg
+     * @param err
+     *
+     */
+    public BadArgError(String msg, Throwable err) {
+        super(msg, err);
+    }
 }
--- game_part/src/main/java/com/game/part/ThreadNamingFactory.java
+++ game_part/src/main/java/com/game/part/ThreadNamingFactory.java
@@ -10,43 +10,43 @@ import java.util.concurrent.ThreadFactory;
  * 
  */
 public class ThreadNamingFactory implements ThreadFactory {
-	/** 线程名称 */
-	private String _tName;
+    /** 线程名称 */
+    private String _tName;
 
-	/**
-	 * 类默认构造器
-	 *
-	 */
-	public ThreadNamingFactory() {
-	}
+    /**
+     * 类默认构造器
+     *
+     */
+    public ThreadNamingFactory() {
+    }
 
-	/**
-	 * 类参数构造器
-	 *
-	 * @param threadName
-	 *
-	 */
-	public ThreadNamingFactory(String threadName) {
-		this.putThreadName(threadName);
-	}
+    /**
+     * 类参数构造器
+     *
+     * @param threadName
+     *
+     */
+    public ThreadNamingFactory(String threadName) {
+        this.putThreadName(threadName);
+    }
 
-	/**
-	 * 设置线程名称
-	 * 
-	 * @param value
-	 * @return 
-	 * 
-	 */
-	public void putThreadName(String value) {
-		this._tName = value;
-	}
+    /**
+     * 设置线程名称
+     *
+     * @param value
+     * @return
+     *
+     */
+    public void putThreadName(String value) {
+        this._tName = value;
+    }
 
-	@Override
-	public Thread newThread(Runnable r) {
-		if (r == null) {
-			return null;
-		} else {
-			return new Thread(r, this._tName);
-		}
-	}
+    @Override
+    public Thread newThread(Runnable r) {
+        if (r == null) {
+            return null;
+        } else {
+            return new Thread(r, this._tName);
+        }
+    }
 }
