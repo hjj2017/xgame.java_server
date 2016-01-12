4e4f9256b6ccd49d0dd8e2df8b61bfd29a7569ed
hjj2017 <hjj2017@163.com>
2016-01-13 07:54:58 +0800

    添加角色创建界面；

---
+++ client_wpf/Xgame.WpfApp/CreateHuman/Page_CreateHuman.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Xgame.WpfApp.CreateHuman
+{
+    /// <summary>
+    /// Page_CreateHuman.xaml 的交互逻辑
+    /// </summary>
+    public partial class Page_CreateHuman : Page
+    {
+        public Page_CreateHuman()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- client_wpf/Xgame.WpfApp/MainWindow.xaml.cs
+++ client_wpf/Xgame.WpfApp/MainWindow.xaml.cs
@@ -4,10 +4,12 @@ using System.Windows;
 
 using LitJson;
 
+using Xgame.GameBizModule.Human.Msg;
 using Xgame.GameBizModule.Login.Msg;
+using Xgame.GameClient.Msg;
 using Xgame.GamePart.Msg;
 using Xgame.GamePart.Msg.Type;
-using Xgame.GameClient.Msg;
+using Xgame.WpfApp.CreateHuman;
 using Xgame.WpfApp.Home;
 
 namespace Xgame.WpfApp
@@ -92,17 +94,61 @@ namespace Xgame.WpfApp
             ClientServer.OBJ.SendCGMsg(cgMSG);
         }
 
-        // @Override
+        /// <summary>
+        /// 处理 GCLogin 消息
+        /// </summary>
+        /// <param name="gcMSG"></param>
         private void Handle_GCLogin(GCLogin gcMSG)
         {
             // 获取登陆成功标志
             bool loginOk = gcMSG._success.GetBoolVal();
 
-            if (loginOk)
+            if (!loginOk)
             {
-                // 跳转到主城页面
-                this.Dispatcher.BeginInvoke(new Action(this.GoToHome));
+                // 如果登陆失败, 
+                // 则直接退出!
+                // TODO : 需要弹出登陆失败提示
+                return;
             }
+
+            // 
+            // 如果登陆成功了, 
+            // 则请求角色入口列表
+            // 
+            // 创建 CG 消息
+            CGQueryHumanEntryList cgMSG = new CGQueryHumanEntryList();
+            cgMSG._serverName = new MsgStr("LM1");
+            // 发送 CG 消息
+            ClientServer.OBJ.AddGCMsgHandler<GCQueryHumanEntryList>(this.Handle_GCQueryHumanEntryList);
+            ClientServer.OBJ.SendCGMsg(cgMSG);
+        }
+
+        /// <summary>
+        /// 处理 GCQueryHumanEntryList 消息
+        /// </summary>
+        /// <param name="gcMSG"></param>
+        private void Handle_GCQueryHumanEntryList(GCQueryHumanEntryList gcMSG)
+        {
+            if (gcMSG._humanEntryList == null 
+             || gcMSG._humanEntryList.Count <= 0)
+            {
+                // 如果没有任何角色, 
+                // 则跳转到角色创建界面
+                this.Dispatcher.BeginInvoke(new Action(this.GotoCreateHuman));
+            }
+            else
+            {
+                // 选择一个角色, 
+                // 进入游戏!
+            }
+        }
+
+        /// <summary>
+        /// 跳转到创建角色界面
+        /// </summary>
+        private void GotoCreateHuman()
+        {
+            this.Content = new Page_CreateHuman();
         }
 
         /// <summary>
