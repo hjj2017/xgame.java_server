6c07e8df88c1fcbd7e9a09b092dc21beb211971e
hjj2017 <hjj2017@163.com>
2016-02-02 07:47:55 +0800

    添加 Attribute 属性类

---
+++ client_wpf/Xgame.GamePart/Tmpl/Attr/OneToManyAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Xgame.GamePart.Tmpl.Attr
+{
+    /// <summary>
+    /// 一个关键字对应多个模板对象
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class OneToManyAttribute : Attribute
+    {
+        #region 类构造器
+        /// <summary>
+        /// 类参数构造器
+        /// </summary>
+        /// <param name="groupName"></param>
+        public OneToOneAttribute(string groupName)
+        {
+            this.GroupName = groupName;
+        }
+        #endregion
+
+        /// <summary>
+        /// 获取或设置分组名称
+        /// </summary>
+        public string GroupName
+        {
+            get;
+            set;
+        }
+    }
+}
