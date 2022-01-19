f3d27635775aff460d0bb3280e7682913aff2199
HAIJIANG JIN <hjj2021@outlook.com>
2022-01-19 22:32:39 +0800

    完善代码逻辑；

--- dbfarmer/src/main/java/org/xgame/dbfarmer/MainApp.java
+++ dbfarmer/src/main/java/org/xgame/dbfarmer/MainApp.java
@@ -1,5 +1,7 @@
 package org.xgame.dbfarmer;
 
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * 应用程序类
  */
@@ -10,5 +12,7 @@ public final class MainApp {
      * @param argvArray 参数数组
      */
     public static void main(String[] argvArray) {
+        // 设置 log4j 属性文件
+        PropertyConfigurator.configure(MainApp.class.getClassLoader().getResourceAsStream("log4j.properties"));
     }
 }
