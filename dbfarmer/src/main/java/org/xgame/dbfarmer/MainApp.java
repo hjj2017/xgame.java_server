19513f8d963746ef9b3173b7d5706867007844af
HAIJIANG JIN <hjj2021@outlook.com>
2022-01-19 22:25:20 +0800

    完善数据库农民代码；

---
+++ dbfarmer/src/main/java/org/xgame/dbfarmer/MainApp.java
@@ -0,0 +1,14 @@
+package org.xgame.dbfarmer;
+
+/**
+ * 应用程序类
+ */
+public final class MainApp {
+    /**
+     * 应用程序主函数
+     *
+     * @param argvArray 参数数组
+     */
+    public static void main(String[] argvArray) {
+    }
+}
